0d1b23ee1578d776d5b912f6f90361f2caa991bf
Lena Ireland <lenaireland@gmail.com>
2018-10-08 18:03:28 -0700

    started working on server.py

--- server.py
+++ server.py
@@ -16,3 +16,27 @@ app.secret_key = "THISISSECRETHERE"
 # silently. This is horrible. Fix this so that, instead, it raises an
 # error.
 # app.jinja_env.undefined = StrictUndefined
+
+@app.route('/')
+def index():
+    """Homepage."""
+
+    return render_template("homepage.html")
+
+
+##############################################################################
+
+if __name__ == "__main__":
+    # We have to set debug=True here, since it has to be True at the
+    # point that we invoke the DebugToolbarExtension
+    app.debug = True
+    # make sure templates, etc. are not cached in debug mode
+    app.jinja_env.auto_reload = app.debug
+
+    connect_to_db(app)
+
+    # Use the DebugToolbar
+    DebugToolbarExtension(app)
+
+    app.run(port=5000, host='0.0.0.0')
+    # app.run(debug=True)
