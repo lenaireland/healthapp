bb36890297d2a655d921643128d94bea7f6a5ac7
Lena Ireland <lenaireland@gmail.com>
2018-11-01 16:58:26 -0700

    Cleaned up D3 plotting code

--- static/js/plot.js
+++ static/js/plot.js
@@ -1,9 +1,19 @@
+
 'use strict';
 
-// set the dimensions and margins of the graph
-const margin = {top: 20, right: 20, bottom: 30, left: 50},
+// adapted example D3 code from
+// https://bl.ocks.org/d3noob/402dd382a51a4f6eea487f9a35566de0
+// and http://bl.ocks.org/d3noob/d8be922a10cb0b148cd5
+// and http://www.d3noob.org/2014/07/d3js-multi-line-graph-with-automatic.html
+
+// set the dimensions and margins of the values graph
+let margin = {top: 20, right: 20, bottom: 70, left: 100},
     width = 960 - margin.left - margin.right,
     height = 500 - margin.top - margin.bottom;
+
+
+// Get today's date
+const now = new Date();
 
 // parse the date / time
 const parseTime = d3.timeParse("%Y-%m-%d");
@@ -12,81 +22,330 @@ const parseTime = d3.timeParse("%Y-%m-%d");
 const x = d3.scaleTime().range([0, width]);
 const y = d3.scaleLinear().range([height, 0]);
 
-// define the line
-const valueline = d3.line()
-    .x(function(d) { return x(d.date); })
-    .y(function(d) { return y(d.value); });
+const color = d3.scaleOrdinal(d3.schemeCategory10);
 
-// append the svg obgect to the body of the page
+// define the line
+let valueline = d3.line()
+  .x(function(d) { return x(d.date); })
+  .y(function(d) { return y(d.value); });
+
+// append the svg object to the body of the page
 // appends a 'group' element to 'svg'
 // moves the 'group' element to the top left margin
-let svg = d3.select("body").append("svg")
+let svg_value = d3.select("body").append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
   .append("g")
     .attr("transform",
           "translate(" + margin.left + "," + margin.top + ")");
 
-// Get the data
+let svg_symp = d3.select("body").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform",
+          "translate(" + margin.left + "," + margin.top + ")");
 
-let data;
+let svg_count = d3.select("body").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform",
+          "translate(" + margin.left + "," + margin.top + ")");
 
+// let valueResults;
+// let countResults;
+
+// let dateMax;
+// let dateMin;
+
+
+// Get the value data
 $.get('get-value-timeseries.json', function (results) {
-  console.log(results);
-  for (let result in results) {
-    // console.log(results[result]);
-    if (result === 'AQI') {
-      data = results[result];
-      console.log(data);
-    }
-  }
-// });
 
-// d3.csv("data.csv", function(error, data) {
-//   if (error) throw error;
-
-  // format the data
-  data.forEach(function(d) {
-      d.date = parseTime(d.date);
-      // d.value = +d.value;
+  // for list input
+  results.forEach(function(d) {
+    d.date = parseTime(d.date);
   });
 
   // Scale the range of the data
-  x.domain(d3.extent(data, function(d) { return d.date; }));
-  y.domain([0, d3.max(data, function(d) { return d.value; })]);
+  x.domain([d3.min(results, function(d) { return d.date; }), d3.timeDay(now)]);
+  y.domain([0, d3.max(results, function(d) { return d.value; })]);
 
-  // Add the valueline path.
-  svg.append("path")
-      .data([data])
+  // create data nest
+  let valueDataNest = d3.nest()
+    .key(function(d) {return d.name; })
+    .entries(results);
+
+  const legendSpace = width/valueDataNest.length;
+
+  // Loop through each symbol/key
+  valueDataNest.forEach(function(d,i) {
+
+    // Add the valueline path.
+    svg_value.append("path")
       .attr("class", "line")
-      .attr("d", valueline)
-      .attr("fill", "none")
-      .attr("stroke", "steelblue")
-      .attr("stroke-width", 1.5);
+      .style("stroke", function() {
+        return d.color = color(d.key); })
+      .attr("id", 'tag'+d.key.replace(/\s+/g, '')) // assign ID
+      .attr("d", valueline(d.values))
+      // .attr("fill", "none")
+      // .attr("stroke", "steelblue")
+      // .attr("stroke-width", 1.5);
+
+    // Add the legend
+    svg_value.append("text")            
+      .attr("x", (legendSpace/2)+i*legendSpace)
+      .attr("y", height + (margin.bottom/2)+ 5)
+      .attr("class", "legend")
+      .style("fill", function() {
+        return d.color = color(d.key); })
+      .on("click", function() {
+        // Determine if current line is visibile
+        let active = d.active ? false : true;
+        let newOpacity = active ? 0 : 1;
+        // Hide or show elements based on ID
+        d3.select("#tag"+d.key.replace(/\s+/g, ''))
+          .transition().duration(100)
+          .style("opacity", newOpacity);
+        // Update whether or not the elements are active
+        d.active = active;
+      })
+      .text(d.key);
+  });
 
   // Add the X Axis
-  svg.append("g")
-      .attr("transform", "translate(0," + height + ")")
-      .call(d3.axisBottom(x));
+  svg_value.append("g")
+     .attr("class", "x axis")
+     .attr("transform", "translate(0," + height + ")")
+     .call(d3.axisBottom(x));
 
   // Add the Y Axis
-  svg.append("g")
-      .call(d3.axisLeft(y));
+  svg_value.append("g")
+     .attr("class", "y axis")
+     .call(d3.axisLeft(y));
+
+});
+
+// Get the symptom data
+$.get('get-symptom-timeseries.json', function (results) {
+
+  // for list input
+  results.forEach(function(d) {
+    d.date = parseTime(d.date);
+  });
+
+  // Scale the range of the data
+  x.domain([d3.min(results, function(d) { return d.date; }), d3.timeDay(now)]);
+
+  const colorDomain = d3.extent(results, function(d) { return d.date; });
+
+  let colorScale = d3.scaleOrdinal(['pink', 'blue', 'orange', 'green']);
+
+  // const colorScale = d3.scaleLinear()
+  //   .domain(colorDomain)
+  //   .range(["lightblue", "blue"]);
+
+  // create data nest
+  let sympDataNest = d3.nest()
+    .key(function(d) {return d.name; })
+    .entries(results);
+
+  let xMin = d3.min(results, function(d) {return d.date});
+  let xMax = d3.max(results, function(d) {return d.date});
+  let yNum = sympDataNest.length;
+
+  let numDays = (xMax - xMin)/1000/3600/24;
+
+  let xScale = d3.scaleLinear()
+                 .domain([xMin, xMax])
+                 .range([0, 800]);
+
+  let yScale = d3.scaleLinear()
+                 .domain([0, yNum])
+                 .range([0, 400]);
+
+  let allDays = Array.from(Array(numDays + 1).keys());
+
+
+  sympDataNest.forEach(function(data, index) {
+
+    let boxes = svg_symp.selectAll("eachSymp")
+      .data(data.values)
+      .enter()
+      .append('g')
+      .attr("class", "rect");
+
+    boxes.append('rect')
+      .attr('x', (d) => xScale(d.date))
+      .attr('y', () => yScale(index))
+      .attr("width", () => (xScale(xMax)/numDays))
+      .attr("height", () => (xScale(xMax)/numDays))
+      .style('fill', (d) => colorScale(d.name));
+
+    allDays.forEach(function(num) {
+
+      let empty_boxes = svg_symp.selectAll("eachSymp")
+        .data(data.values)
+        .enter()
+        .append('g')
+        .attr("class", "rect");
+
+      empty_boxes.append('rect')
+        .attr('x', () => xScale(xMax)/numDays*num )
+        .attr('y', () => yScale(index))
+        .attr("width", () => (xScale(xMax)/numDays))
+        .attr("height", () => (xScale(xMax)/numDays))
+        .style('fill', 'transparent')
+        .style("stroke", 'black')
+        .style("stroke-width", 0.5);
+
+      empty_boxes.append('text')
+        .attr('x', -100)
+        .attr('y', (yScale(index) + (xScale(xMax)/numDays/2)))
+        .text((d) => d.name);
+    })
+
+  });
+
+
+  // Add the X Axis
+  svg_symp.append("g")
+     .attr("class", "x axis")
+     .attr("transform", "translate(0," + height + ")")
+     .call(d3.axisBottom(x));
+});
+
+
+// Get the count data
+$.get('get-count-timeseries.json', function (results) {
+
+  // for list input
+  results.forEach(function(d) {
+    d.date = parseTime(d.date);
+  });
+
+  // Scale the range of the data
+  x.domain([d3.min(results, function(d) { return d.date; }), d3.timeDay(now)]);
+  // y.domain([0, d3.max(results, function(d) { return d.count; })]);
+
+
+  const colorDomain = d3.extent(results, function(d) { return d.date; });
+
+  let colorScale = d3.scaleOrdinal(['pink', 'blue', 'orange', 'green']);
+
+  // const colorScale = d3.scaleLinear()
+  //   .domain(colorDomain)
+  //   .range(["lightblue", "blue"]);
+
+  // create data nest
+  let countDataNest = d3.nest()
+    .key(function(d) {return d.name; })
+    .entries(results);
+
+  let xMin = d3.min(results, function(d) {return d.date});
+  let xMax = d3.max(results, function(d) {return d.date});
+  let yNum = countDataNest.length;
+
+  let numDays = (xMax - xMin)/1000/3600/24;
+
+  let xScale = d3.scaleLinear()
+                 .domain([xMin, xMax])
+                 .range([0, 800]);
+
+  let yScale = d3.scaleLinear()
+                 .domain([0, yNum])
+                 .range([0, 400]);
+
+  let allDays = Array.from(Array(numDays + 1).keys());
+
+
+  countDataNest.forEach(function(data, index) {
+
+    // count_height += 100;
+
+    // let label = svg_count.selectAll("labels")
+    //   .data(data)
+    //   .enter()
+    //   .append('g')
+    //   .attr("class", "name");
+
+    // label.append('name')
+    //   .attr('x', -150)
+    //   .attr('y', (d) => (100 * index+25))
+    //   .text((d) => d.key);
+
+    let boxes = svg_count.selectAll("eachCount")
+      .data(data.values)
+      .enter()
+      .append('g')
+      .attr("class", "rect");
+
+    boxes.append('rect')
+      .attr('x', (d) => xScale(d.date))
+      .attr('y', () => yScale(index))
+      .attr("width", () => (xScale(xMax)/numDays))
+      .attr("height", () => (xScale(xMax)/numDays))
+      .style('fill', (d) => colorScale(d.name));
+
+    boxes.append('text')
+      .attr('x', (d) => (xScale(d.date) + (xScale(xMax)/numDays/2) ))
+      .attr('y', () => (yScale(index) + (xScale(xMax)/numDays/2) ))
+      .text((d) => d.count);
+
+    allDays.forEach(function(num) {
+
+      let empty_boxes = svg_count.selectAll("eachCount")
+        .data(data.values)
+        .enter()
+        .append('g')
+        .attr("class", "rect");
+
+      empty_boxes.append('rect')
+        .attr('x', () => xScale(xMax)/numDays*num )
+        .attr('y', () => yScale(index))
+        .attr("width", () => (xScale(xMax)/numDays))
+        .attr("height", () => (xScale(xMax)/numDays))
+        .style('fill', 'transparent')
+        .style("stroke", 'black')
+        .style("stroke-width", 0.5);
+
+      empty_boxes.append('text')
+        .attr('x', -100)
+        .attr('y', (yScale(index) + (xScale(xMax)/numDays/2)))
+        .text((d) => d.name);
+    })
+
+  });
+
+
+  // Add the X Axis
+  svg_count.append("g")
+     .attr("class", "x axis")
+     .attr("transform", "translate(0," + height + ")")
+     .call(d3.axisBottom(x));
+
+  // // Add the Y Axis
+  // svg_count.append("g")
+  //    .attr("class", "y axis")
+  //    .call(d3.axisLeft(y));
 
 });
 
 
-// event listener called when the DOM is ready
-$(document).ready($.get('get-symptom-timeseries.json', function (results) {
-  console.log(results);
-}));
 
-// event listener called when the DOM is ready
-$(document).ready($.get('get-value-timeseries.json', function (results) {
-  console.log(results);
-}));
 
-// event listener called when the DOM is ready
-$(document).ready($.get('get-count-timeseries.json', function (results) {
-  console.log(results);
-}));
+// // event listener called when the DOM is ready
+// $(document).ready($.get('get-symptom-timeseries.json', function (results) {
+//   console.log(results);
+// }));
+
+// // event listener called when the DOM is ready
+// $(document).ready($.get('get-value-timeseries.json', function (results) {
+//   console.log(results);
+// }));
+
+// // event listener called when the DOM is ready
+// $(document).ready($.get('get-count-timeseries.json', function (results) {
+//   console.log(results);
+// }));
