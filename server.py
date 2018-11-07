084c5f19ed1cf26fb367ddd1b90284a040f6b19a
Lena Ireland <lenaireland@gmail.com>
2018-11-07 14:21:44 -0800

    Updated fonts

--- server.py
+++ server.py
@@ -1333,10 +1333,10 @@ def user_not_tracked_count_types():
 if __name__ == "__main__":
     # We have to set debug=True here, since it has to be True at the
     # point that we invoke the DebugToolbarExtension
-    # app.debug = True
+    app.debug = True
     
     # make sure templates, etc. are not cached in debug mode
-    # app.jinja_env.auto_reload = app.debug
+    app.jinja_env.auto_reload = app.debug
 
     connect_to_db(app)
 
