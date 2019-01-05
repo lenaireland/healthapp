565a66233a0797f8d58a10b0926bfc2b9c3a1cdb
Lena Ireland <lenaireland@gmail.com>
2019-01-05 00:24:26 +0000

    Turned off debug mode

--- server.py
+++ server.py
@@ -1332,17 +1332,17 @@ def user_not_tracked_count_types():
 if __name__ == "__main__":
     # We have to set debug=True here, since it has to be True at the
     # point that we invoke the DebugToolbarExtension
-    app.debug = True
+    #app.debug = True
     
     # make sure templates, etc. are not cached in debug mode
-    app.jinja_env.auto_reload = app.debug
+    #app.jinja_env.auto_reload = app.debug
     
     connect_to_db(app)
     # Use the DebugToolbar
     DebugToolbarExtension(app)
  
     app.run(port=5000, host='0.0.0.0')
-    app.run(debug=True)
+    #app.run(debug=True)
 
 
 # TRYING TO MAKE IT SO DON'T HAVE TO TURN OF FLASK DEBUGGER - NOT WORKING
